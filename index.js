8cebdaf1f598b14cd63c926f23a7d3ad64e20eb2
codingmohamed <cloudchronicles92@gmail.com>
2022-01-06 12:47:29 +0000

    last task left

--- index.js
+++ index.js
@@ -1,19 +1,43 @@
 // Arrays:
 
 // Immutably add an item to the end of an array
-export function addToEnd(array, item) {}
+export function addToEnd(array, item) {
+      return [...array, item];
+}
 
 // Immutably add an item to the beginning of an array
-export function addToBeginning(array, item) {}
+export function addToBeginning(array, item) {
+      return [item, ...array];
+}
 
 // Immutably insert an item at a specific position/index within an array
-export function insertItem(array, item, index) {}
+export function insertItem(array, item, index) {
+      const slicedArray = [
+            ...array.slice(0, index),
+            item,
+            ...array.slice(index),
+      ];
+      return slicedArray;
+}
 
 // Immutably replace an item at a specific position/index within an array
-export function replaceItem(array, item, index) {}
+export function replaceItem(array, item, index) {
+      const replacedArray = [
+            ...array.slice(0, index),
+            item,
+            ...array.slice(index + 1),
+      ];
+      return replacedArray;
+}
 
 // Immutably remove an item at a specific position/index within an array
-export function removeItem(array, index) {}
+export function removeItem(array, index) {
+      const removedArray = [
+            ...array.slice(0, index),
+            ...array.slice(index + 1),
+      ];
+      return removedArray;
+}
 
 //Objects:
 
@@ -22,7 +46,10 @@ export function removeItem(array, index) {}
 //     updateName({ name: "Abe" }, "Barbara")
 // should give back:
 //     { name: "Barbara"}
-export function updateName(object, newName) {}
+export function updateName(object, newName) {
+      const newObject = { ...object, name: newName };
+      return newObject;
+}
 
 // Immutably update the object so that the value under the "needsACupOfTea" property becomes the opposite of what it was.
 // Any other properties in the object should be maintained.
@@ -30,7 +57,10 @@ export function updateName(object, newName) {}
 //     toggleTeaStatus({ name: "Abe", needsACupOfTea: false })
 // should give back:
 //     { name: "Abe", needsACupOfTea: true }
-export function toggleTeaStatus(object) {}
+export function toggleTeaStatus(object) {
+      const cupOfTea = { ...object, needsACupOfTea: !object.needsACupOfTea };
+      return cupOfTea;
+}
 
 // Combo Time!!
 
@@ -40,4 +70,6 @@ export function toggleTeaStatus(object) {}
 //    toggleListItemCompleted([{ task: "Cooking", completed: true }, { task: "Walking", completed: false }], 1)
 // should give back:
 //    [{ task: "Cooking", completed: true }, { task: "Walking", completed: true }]
-export function toggleListItemCompleted(array, index) {}
+export function toggleListItemCompleted(array, index) {
+      const oppositeComplete = [...array, array[index].completed:!object.completed]
+}
